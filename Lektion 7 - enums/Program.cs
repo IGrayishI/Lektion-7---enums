e3e6c2803a0d18579492d1dde7656175c529cbb1
Benjamin Kjellgren Kuurne <benjamin.kjellgren.kuurne@iths.se>
2023-09-11 10:28:39 +0200

    Added some comments regarding the lession so far.

--- Lektion 7 - enums/Program.cs
+++ Lektion 7 - enums/Program.cs
@@ -6,6 +6,10 @@
         {
             //Enums
 
+
+            //Test for Github Pushing.
+            //Why dont we just add some Enums code instead of doing redundancies?
+            
         }
     }
 }
