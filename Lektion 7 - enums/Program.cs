98caef3d570c958c0b2dce0653db16b278400ad6
Benja <Benja@DESKTOP-R0V7P5O>
2023-09-11 09:58:04 +0200

    Add project files.

---
+++ Lektion 7 - enums/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lektion_7___enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Benjo! Time to code!");
+            //Enums
+        }
+    }
+}
